108d74c70f736138b6a2a55ac19c3c063ff7c4fd
Chris Westin <cwestin@yahoo.com>
2015-03-22 18:30:18 -0700

    Added sample files for figuring out accounting needs.

---
+++ java-samples/src/com/bookofbrilliantthings/callgrind4jsamples/SomeClass.java
@@ -0,0 +1,13 @@
+// TODO license header
+package com.bookofbrilliantthings.callgrind4jsamples;
+
+public class SomeClass {
+	public void a() {
+	}
+
+	public void b() {
+	}
+
+	public void c() {
+	}
+}
